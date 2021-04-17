65f61bfdd756c59553b7952f0b966cfb9480daee
ptylek <pawel.tylek@icloud.com>
2021-04-17 23:47:48 +0200

    feat(docs): update readme.md and use .env variable for api key

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,94 +1,108 @@
 import * as actionTypes from './actionTypes';
 import axios from 'axios';
-import * as firebase from 'constants/firebase';
 
 export const authStart = () => {
 	return {
-		type: actionTypes.AUTH_START
-	}
-}
+		type: actionTypes.AUTH_START,
+	};
+};
 
 export const authSuccess = (idToken, userId) => {
 	return {
 		type: actionTypes.AUTH_SUCCESS,
 		idToken: idToken,
-		userId: userId
-	}
-}
+		userId: userId,
+	};
+};
 
 export const authFail = (error) => {
 	return {
 		type: actionTypes.AUTH_FAIL,
-		error: error
-	}
-}
+		error: error,
+	};
+};
 
 export const logout = () => {
 	localStorage.removeItem('token');
 	localStorage.removeItem('expirationDate');
 	localStorage.removeItem('userId');
-	
+
 	return {
-		type: actionTypes.AUTH_LOGOUT
-	}
-}
+		type: actionTypes.AUTH_LOGOUT,
+	};
+};
 
 export const checkAuthTimeout = (expirationTime) => {
-	return dispatch => {
+	return (dispatch) => {
 		setTimeout(() => {
 			dispatch(logout());
-		}, expirationTime * 1000)
-	}
-}
+		}, expirationTime * 1000);
+	};
+};
 
 export const auth = (email, password, isRegister) => {
-	return dispatch => {
+	return (dispatch) => {
 		dispatch(authStart());
 
 		const authData = {
 			email: email,
 			password: password,
-			returnSecureToken: true
-		}
+			returnSecureToken: true,
+		};
 
-		let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + firebase.API_KEY;
+		let url =
+			'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' +
+			process.env.REACT_APP_FIREBASE_API_KEY;
 
 		if (!isRegister) {
-			url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + firebase.API_KEY;
+			url =
+				'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' +
+				process.env.REACT_APP_FIREBASE_API_KEY;
 		}
 
-		axios.post(url, authData)
-			.then(response => {
-				const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
-				
+		axios
+			.post(url, authData)
+			.then((response) => {
+				const expirationDate = new Date(
+					new Date().getTime() + response.data.expiresIn * 1000
+				);
+
 				localStorage.setItem('token', response.data.idToken);
-				localStorage.setItem('expirationDate', expirationDate)
-				localStorage.setItem('userId', response.data.localId)
-				dispatch(authSuccess(response.data.idToken, response.data.localId));
+				localStorage.setItem('expirationDate', expirationDate);
+				localStorage.setItem('userId', response.data.localId);
+				dispatch(
+					authSuccess(response.data.idToken, response.data.localId)
+				);
 				dispatch(checkAuthTimeout(response.data.expiresIn));
 			})
-			.catch(error => {
+			.catch((error) => {
 				dispatch(authFail(error.response.data.error.message));
 			});
-	}
-}
+	};
+};
 
 export const authCheck = () => {
-	return dispatch => {
+	return (dispatch) => {
 		const token = localStorage.getItem('token');
 
 		if (!token) {
 			dispatch(logout());
 		} else {
-			const expirationDate = new Date(localStorage.getItem('expirationDate'));
-			
+			const expirationDate = new Date(
+				localStorage.getItem('expirationDate')
+			);
+
 			if (expirationDate <= new Date()) {
 				dispatch(logout());
 			} else {
 				const userId = localStorage.getItem('userId');
 				dispatch(authSuccess(token, userId));
-				dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime()) / 1000));
+				dispatch(
+					checkAuthTimeout(
+						(expirationDate.getTime() - new Date().getTime()) / 1000
+					)
+				);
 			}
 		}
-	}
-}
+	};
+};
